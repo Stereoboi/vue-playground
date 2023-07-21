5debc891d42e329a72bfd3660fb00a5f1d114850
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-07-21 15:26:47 +0300

    wierd staff

---
+++ src/components/List.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const items = ref(["My", "name", "is", "Bohdan"]);
+</script>
+
+<template>
+  <li class="list-items" v-for="el in items">{{ el }}</li>
+</template>
+
+<style>
+.list-items {
+  display: flex;
+}
+</style>
